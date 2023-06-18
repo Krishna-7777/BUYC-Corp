490f1b1c8ee781a7f0ee4dcea675ec3ced6dc72c
Krishna-7777 <kchhabraji13@gmail.com>
2023-06-18 20:38:06 +0530

    get years api

--- backend/route/oem.route.js
+++ backend/route/oem.route.js
@@ -3,22 +3,28 @@ const { OemModel } = require("../models/oem.model");
 
 const OemRoutes = express.Router();
 
-OemRoutes.post('/search', async (req, res) => {
-    const searchTerms = req.body 
+OemRoutes.post('/search', async (ask, give) => {
+    const searchTerms = ask.body 
     try {
         const cars = await OemModel.find({
             Make: { $regex: searchTerms[0],$options:'i' },
             Year: Number(searchTerms[2]) ,
             Model: { $regex: searchTerms[1] ,$options:'i'}
           })
-
-      res.send(cars);
+      give.send(cars);
     } catch (error) {
         console.log(error)
-    }
-    
-    }) 
+    }}) 
 
+OemRoutes.get('/distinct-years', async (ask, give) => {
+    try {
+        const distinctYears = await OemModel.distinct('Year')
+        give.send(distinctYears);
+    } catch (error) {
+        console.error(error);
+        give.status(500).json({ error: 'Internal server error' });
+    }
+});
 
   module.exports={
     OemRoutes
