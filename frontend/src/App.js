913521735d9533e30368dff0370c931c97db56d9
Krishna-7777 <kchhabraji13@gmail.com>
2023-06-18 12:37:49 +0530

    Added Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./components/Navbar";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( <BrowserRouter>
+    <NavBar />
+    <Routes>
+    <Route path="/" element={<div>Home Page</div>}  />
+    <Route path="/register" element={<div>Register</div>} />
+    <Route path="/login" element={<div>Login Page</div>} />
+    <Route path="*" element={<div>Page not Found</div>}/>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
